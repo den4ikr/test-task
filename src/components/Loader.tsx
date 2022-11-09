cc6f075eba2a72f987e81021525a5d9066c8257f
Danik Roman <danirom0401@gmail.com>
2022-11-09 13:05:27 +0200

    General: remove npm lock file, centrify loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,15 @@ import { Box, CircularProgress } from "@mui/material";
 
 const Loader: React.FC = () => {
   return (
-    <Box sx={{ width: "100%", height: "100vh" }}>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <CircularProgress />
     </Box>
   );
